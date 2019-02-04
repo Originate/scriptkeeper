fca6c7978882acac74ccd1f0c9bfe216159b0711
soenkehahn <SoenkeHahn@gmail.com>
2019-02-04 17:35:57 -0500

    move peekuser

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,24 @@
+use libc::c_long;
+use nix::unistd::Pid;
+use std::ffi::c_void;
+use std::ptr::null_mut;
 
+pub type R<A> = Result<A, Box<std::error::Error>>;
+
+pub fn peekuser(child: Pid) -> R<()> {
+    let offset = (libc::ORIG_RAX * 8) as *mut c_void;
+    #[allow(deprecated)]
+    unsafe {
+        let register: c_long = libc::ptrace(
+            libc::PTRACE_PEEKUSER,
+            child,
+            offset,
+            null_mut() as *mut c_void,
+        );
+        println!(
+            "offset: {:?} - {}, register: {}",
+            offset, offset as u64, register
+        );
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use libc::c_long;
 use nix::sys::ptrace;
 use nix::sys::wait::wait;
-use nix::unistd::{execv, fork, ForkResult, Pid};
-use std::ffi::{c_void, CString};
-use std::ptr::null_mut;
-
-type R<A> = Result<A, Box<std::error::Error>>;
+use nix::unistd::{execv, fork, ForkResult};
+use std::ffi::CString;
+use tracing_poc::{peekuser, R};
 
 fn main() -> R<()> {
     let result = fork()?;
@@ -16,27 +13,9 @@ fn main() -> R<()> {
         }
         ForkResult::Parent { child } => {
             println!("wait result: {:?}", wait());
-            rust_peekuser(child)?;
+            peekuser(child)?;
             ptrace::cont(child, None)?;
         }
-    }
-    Ok(())
-}
-
-fn rust_peekuser(child: Pid) -> R<()> {
-    let offset = (libc::ORIG_RAX * 8) as *mut c_void;
-    #[allow(deprecated)]
-    unsafe {
-        let register: c_long = libc::ptrace(
-            libc::PTRACE_PEEKUSER,
-            child,
-            offset,
-            null_mut() as *mut c_void,
-        );
-        println!(
-            "offset: {:?} - {}, register: {}",
-            offset, offset as u64, register
-        );
     }
     Ok(())
 }
