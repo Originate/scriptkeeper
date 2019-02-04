d728cfdbbf6678ae651365d68a87d0431aabf6a7
soenkehahn <SoenkeHahn@gmail.com>
2019-02-04 17:41:23 -0500

    add test for peekuser

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::ptr::null_mut;
 
 pub type R<A> = Result<A, Box<std::error::Error>>;
 
-pub fn peekuser(child: Pid) -> R<()> {
+pub fn peekuser(child: Pid) -> R<c_long> {
     let offset = (libc::ORIG_RAX * 8) as *mut c_void;
     #[allow(deprecated)]
     unsafe {
@@ -19,6 +19,35 @@ pub fn peekuser(child: Pid) -> R<()> {
             "offset: {:?} - {}, register: {}",
             offset, offset as u64, register
         );
+        Ok(register)
     }
-    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use nix::sys::ptrace;
+    use nix::sys::wait::wait;
+    use nix::unistd::{execv, fork, ForkResult};
+    use std::ffi::CString;
+
+    #[test]
+    fn tracks_syscalls() {
+        let result = fork().unwrap();
+        match result {
+            ForkResult::Child => {
+                ptrace::traceme().unwrap();
+                execv(
+                    &CString::new("./foo").unwrap(),
+                    &vec![CString::new("./foo").unwrap()],
+                )
+                .unwrap();
+            }
+            ForkResult::Parent { child } => {
+                println!("wait result: {:?}", wait());
+                assert_eq!(peekuser(child).unwrap(), 59);
+                ptrace::cont(child, None).unwrap();
+            }
+        }
+    }
 }
