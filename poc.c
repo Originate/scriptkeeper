fa26fb989958393eff50cca7f31ea168c2e72890
soenkehahn <SoenkeHahn@gmail.com>
2019-02-04 14:51:48 -0500

    wip

---
+++ poc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/reg.h>   /* For constants
+                                   ORIG_EAX etc */
+int main()
+{   pid_t child;
+    long orig_rax;
+    child = fork();
+    if(child == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execl("/bin/ls", "ls", NULL);
+    }
+    else {
+        wait(NULL);
+        printf("offset: %i\n", 8 * ORIG_RAX);
+        orig_rax = ptrace(PTRACE_PEEKUSER,
+                          child, 8 * ORIG_RAX,
+                          NULL);
+        printf("The child made a "
+               "system call %ld\n", orig_rax);
+        ptrace(PTRACE_CONT, child, NULL, NULL);
+    }
+    return 0;
+}
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use libc::{c_long, user_regs_struct};
+use nix::sys::ptrace;
+use nix::sys::ptrace::Request;
+// use nix::sys::wait::wait;
+use nix::unistd::{fork, ForkResult, Pid};
+use std::ffi::{c_void, CString};
+use std::mem::size_of;
+use std::ptr::{null, null_mut};
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let result = fork()?;
+    match result {
+        ForkResult::Child => {
+            unsafe {
+                println!(
+                    "traceme: {:?}",
+                    ptrace::ptrace(
+                        Request::PTRACE_TRACEME,
+                        Pid::from_raw(0),
+                        null_mut(),
+                        null_mut()
+                    )?
+                );
+            }
+            let path = CString::new("/bin/ls")?;
+            let path_ptr: *const i8 = path.as_ptr();
+            let arg0 = CString::new("ls")?;
+            let end_marker: *const i8 = null();
+            unsafe {
+                libc::execl(path_ptr, arg0.as_ptr(), end_marker);
+            }
+        }
+        ForkResult::Parent { child } => {
+            println!("parent... ({})", child);
+            println!("size: {:?}", size_of::<user_regs_struct>() / 8);
+            unsafe {
+                println!("wait result: {:?}", libc::wait(null_mut()));
+            }
+            let i = 15;
+            let offset = (i * 8) as *mut c_void;
+            unsafe {
+                #[allow(deprecated)]
+                let register: c_long =
+                    ptrace::ptrace(Request::PTRACE_PEEKUSER, child, offset, null_mut())?;
+                println!(
+                    "offset: {:?} - {}, register: {}",
+                    offset, offset as u64, register
+                );
+                ptrace::ptrace(Request::PTRACE_CONT, child, null_mut(), null_mut());
+            };
+        }
+    }
+    Ok(())
+}
