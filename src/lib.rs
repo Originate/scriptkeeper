9a8570271db050aca99d52c7c8d7b5b25aa10c21
soenkehahn <SoenkeHahn@gmail.com>
2019-02-06 12:03:43 -0500

    implement propagation of child errors to the parent

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ use nix::sys::wait::{waitpid, WaitStatus};
 use nix::unistd::Pid;
 use nix::unistd::{execv, fork, getpid, ForkResult};
 use std::ffi::CString;
+use std::fs::{read_to_string, write};
+use tempdir::TempDir;
 
 pub type R<A> = Result<A, Box<std::error::Error>>;
 
@@ -46,16 +48,134 @@ mod test_data_to_string {
     }
 }
 
-pub fn first_execve_path(executable: impl ToString) -> R<String> {
-    let mut result = None;
+pub fn fork_with_child_errors<A>(
+    child_action: impl FnOnce() -> R<()>,
+    parent_action: impl Fn(Pid) -> R<A>,
+) -> R<A> {
+    let tempdir = TempDir::new("tracing-poc")?;
+    let error_file_path = tempdir.path().join("error");
+    write(&error_file_path, "")?;
     match fork()? {
         ForkResult::Child => {
+            Box::leak(Box::new(tempdir));
+            let result: R<()> = (|| -> R<()> {
+                child_action()?;
+                Err("child_action: please either exec or fail".to_string())?;
+                Ok(())
+            })();
+            match result {
+                Ok(()) => {}
+                Err(error) => write(&error_file_path, format!("{}", error))?,
+            };
+            std::process::exit(0);
+        }
+        ForkResult::Parent { child } => {
+            let result = parent_action(child)?;
+            match read_to_string(&error_file_path)?.as_str() {
+                "" => Ok(result),
+                error => Err(error)?,
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_fork_with_child_errors {
+    use super::*;
+    use std::fs::{read_to_string, write};
+    use tempdir::TempDir;
+
+    #[test]
+    fn runs_the_child_action() -> R<()> {
+        let tempdir = TempDir::new("test")?;
+        let temp_file_path = tempdir.path().join("foo");
+        fork_with_child_errors(
+            || {
+                write(&temp_file_path, "bar")?;
+                execv(&CString::new("/bin/true")?, &vec![])?;
+                Ok(())
+            },
+            |child: Pid| {
+                loop {
+                    match waitpid(child, None)? {
+                        WaitStatus::Exited(..) => break,
+                        _ => {}
+                    }
+                }
+                Ok(())
+            },
+        )?;
+        assert_eq!(read_to_string(&temp_file_path)?, "bar");
+        Ok(())
+    }
+
+    #[test]
+    fn raises_child_errors_in_the_parent() {
+        let result: R<()> = fork_with_child_errors(
+            || {
+                Err("test error")?;
+                Ok(())
+            },
+            |child: Pid| {
+                loop {
+                    match waitpid(child, None)? {
+                        WaitStatus::Exited(..) => break,
+                        _ => {}
+                    }
+                }
+                Ok(())
+            },
+        );
+        assert_eq!(format!("{}", result.unwrap_err()), "test error");
+    }
+
+    #[test]
+    fn raises_parent_errors_in_the_parent() {
+        let result: R<()> = fork_with_child_errors(
+            || {
+                execv(&CString::new("/bin/true")?, &vec![])?;
+                Ok(())
+            },
+            |_child: Pid| {
+                Err("test error")?;
+                Ok(())
+            },
+        );
+        assert_eq!(format!("{}", result.unwrap_err()), "test error");
+    }
+
+    #[test]
+    fn raises_an_error_when_the_child_does_not_exec() {
+        let result: R<()> = fork_with_child_errors(
+            || Ok(()),
+            |child: Pid| {
+                loop {
+                    match waitpid(child, None)? {
+                        WaitStatus::Exited(..) => break,
+                        _ => {}
+                    }
+                }
+                Ok(())
+            },
+        );
+        assert_eq!(
+            format!("{}", result.unwrap_err()),
+            "child_action: please either exec or fail"
+        );
+    }
+}
+
+pub fn first_execve_path(executable: impl ToString) -> R<String> {
+    fork_with_child_errors(
+        || {
             ptrace::traceme()?;
             signal::kill(getpid(), Some(Signal::SIGSTOP))?;
             let path = CString::new(executable.to_string())?;
             execv(&path, &[path.clone()])?;
-        }
-        ForkResult::Parent { child } => {
+            Ok(())
+        },
+        |child: Pid| -> R<String> {
+            let mut result = None;
             waitpid(child, None)?;
             ptrace::setoptions(child, Options::PTRACE_O_TRACESYSGOOD)?;
             ptrace::syscall(child)?;
@@ -78,9 +198,9 @@ pub fn first_execve_path(executable: impl ToString) -> R<String> {
                 }
                 ptrace::syscall(child)?;
             }
-        }
-    }
-    Ok(result.ok_or("execve didn't happen")?)
+            Ok(result.ok_or("execve didn't happen")?)
+        },
+    )
 }
 
 #[cfg(test)]
@@ -91,4 +211,12 @@ mod test_first_execve_path {
     fn returns_the_path_of_the_spawned_executable() {
         assert_eq!(first_execve_path("./foo").unwrap(), "./foo");
     }
+
+    // #[test]
+    // fn complains_when_the_file_does_not_exist() {
+    //     assert_eq!(
+    //         format!("{:?}", first_execve_path("./does_not_exist")),
+    //         "ENOENT: file not found: ./does_not_exist"
+    //     );
+    // }
 }
