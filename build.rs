ade723f2f92b5366b6fef5d31d41c3cca8a7b9ca
Sönke Hahn <soenkehahn@gmail.com>
2019-02-07 23:10:02 -0500

    implement peekdata completely in rust

--- build.rs
+++
@@ -1,7 +0,0 @@
-extern crate cc;
-
-fn main() {
-    cc::Build::new()
-        .file("src/c-tracing-poc.c")
-        .compile("c-tracing-poc");
-}
--- src/c-tracing-poc.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <sys/ptrace.h>
-#include <sys/types.h>
-
-long c_ptrace_peekdata(pid_t child, long address) {
-  return ptrace(PTRACE_PEEKDATA, child, address, NULL);
-}
