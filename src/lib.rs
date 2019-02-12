8fd4948899d9505a84eb713f685a44c18a03984f
soenkehahn <SoenkeHahn@gmail.com>
2019-02-12 16:04:18 -0500

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,255 +1,54 @@
 #![cfg_attr(feature = "ci", deny(warnings))]
 
+mod syscall_mocking;
 mod tracee_memory;
 
+use crate::syscall_mocking::run_against_mock;
 use crate::tracee_memory::{data_to_string, ptrace_peekdata_iter, ptrace_pokedata, string_to_data};
-use libc::c_ulonglong;
-use nix::sys::ptrace;
-use nix::sys::ptrace::Options;
-use nix::sys::signal;
-use nix::sys::signal::Signal;
-use nix::sys::wait::{wait, waitpid, WaitStatus};
+use libc::{c_ulonglong, user_regs_struct};
 use nix::unistd::Pid;
-use nix::unistd::{execv, fork, getpid, ForkResult};
-use std::ffi::CString;
-use std::fs::{copy, read_to_string, write};
-use std::os::unix::ffi::OsStrExt;
-use std::panic;
+use std::fs::copy;
 use std::path::{Path, PathBuf};
-use tempdir::TempDir;
 
 pub type R<A> = Result<A, Box<std::error::Error>>;
 
-pub fn fork_with_child_errors<A>(
-    child_action: impl FnOnce() -> R<()> + panic::UnwindSafe,
-    parent_action: impl Fn(Pid) -> R<A>,
-) -> R<A> {
-    let tempdir = TempDir::new("tracing-poc")?;
-    let error_file_path = tempdir.path().join("error");
-    write(&error_file_path, "")?;
-    match fork()? {
-        ForkResult::Child => {
-            Box::leak(Box::new(tempdir));
-            let result: R<()> = (|| -> R<()> {
-                match panic::catch_unwind(child_action) {
-                    Ok(r) => r,
-                    Err(err) => match err.downcast_ref::<&str>() {
-                        None => Err("child panicked with an unsupported type")?,
-                        Some(str) => Err(*str)?,
-                    },
-                }?;
-                Err("child_action: please either exec or fail".to_string())?;
-                Ok(())
-            })();
-            match result {
-                Ok(()) => {}
-                Err(error) => write(&error_file_path, format!("{}", error))?,
-            };
-            std::process::exit(0);
-        }
-        ForkResult::Parent { child } => {
-            let result = parent_action(child);
-            match read_to_string(&error_file_path)?.as_str() {
-                "" => result,
-                error => Err(error)?,
-            }
-        }
-    }
+pub struct SyscallMock {
+    parent_pid: Pid,
+    execve_paths: Vec<PathBuf>,
 }
 
-#[cfg(test)]
-mod test_fork_with_child_errors {
-    use super::*;
-
-    #[test]
-    fn runs_the_child_action() -> R<()> {
-        let tempdir = TempDir::new("test")?;
-        let temp_file_path = tempdir.path().join("foo");
-        fork_with_child_errors(
-            || {
-                write(&temp_file_path, "bar")?;
-                execv(&CString::new("/bin/true")?, &vec![])?;
-                Ok(())
-            },
-            |child: Pid| {
-                loop {
-                    match waitpid(child, None)? {
-                        WaitStatus::Exited(..) => break,
-                        _ => {}
-                    }
-                }
-                Ok(())
-            },
-        )?;
-        assert_eq!(read_to_string(&temp_file_path)?, "bar");
-        Ok(())
+impl SyscallMock {
+    fn new(parent_pid: Pid) -> SyscallMock {
+        SyscallMock {
+            parent_pid,
+            execve_paths: vec![],
+        }
     }
 
-    #[test]
-    fn raises_child_errors_in_the_parent() {
-        let result: R<()> = fork_with_child_errors(
-            || {
-                Err("test error")?;
-                Ok(())
-            },
-            |child: Pid| {
-                loop {
-                    match waitpid(child, None)? {
-                        WaitStatus::Exited(..) => break,
-                        _ => {}
-                    }
-                }
-                Ok(())
-            },
-        );
-        assert_eq!(format!("{}", result.unwrap_err()), "test error");
-    }
-
-    #[test]
-    fn raises_child_panics_in_the_parent() {
-        let result: R<()> = fork_with_child_errors(
-            || {
-                panic!("test panic");
-            },
-            |child: Pid| {
-                loop {
-                    match waitpid(child, None)? {
-                        WaitStatus::Exited(..) => break,
-                        _ => {}
-                    }
-                }
-                Ok(())
-            },
-        );
-        assert_eq!(format!("{}", result.unwrap_err()), "test panic");
-    }
-
-    #[test]
-    fn raises_parent_errors_in_the_parent() {
-        let result: R<()> = fork_with_child_errors(
-            || {
-                execv(&CString::new("/bin/true")?, &vec![])?;
-                Ok(())
-            },
-            |child: Pid| {
-                loop {
-                    match waitpid(child, None)? {
-                        WaitStatus::Exited(..) => break,
-                        _ => {}
-                    }
-                }
-                Err("test error")?;
-                Ok(())
-            },
-        );
-        assert_eq!(format!("{}", result.unwrap_err()), "test error");
-    }
-
-    #[test]
-    fn raises_an_error_when_the_child_does_not_exec() {
-        let result: R<()> = fork_with_child_errors(
-            || Ok(()),
-            |child: Pid| {
-                loop {
-                    match waitpid(child, None)? {
-                        WaitStatus::Exited(..) => break,
-                        _ => {}
-                    }
-                }
-                Ok(())
-            },
-        );
-        assert_eq!(
-            format!("{}", result.unwrap_err()),
-            "child_action: please either exec or fail"
-        );
-    }
-
-    #[test]
-    fn child_errors_take_precedence_over_parent_errors() {
-        let result: R<()> = fork_with_child_errors(
-            || {
-                Err("test child error")?;
-                Ok(())
-            },
-            |child: Pid| {
-                loop {
-                    match waitpid(child, None)? {
-                        WaitStatus::Exited(..) => break,
-                        _ => {}
-                    }
-                }
-                Err("test parent error")?;
-                Ok(())
-            },
-        );
-        assert_eq!(format!("{}", result.unwrap_err()), "test child error");
-    }
-}
-
-fn get_execve_path(status: &WaitStatus, script_pid: Pid) -> R<Option<PathBuf>> {
-    if let WaitStatus::PtraceSyscall(pid, ..) = status {
-        let registers = ptrace::getregs(*pid)?;
+    fn handle_syscall(&mut self, pid: Pid, registers: user_regs_struct) -> R<()> {
         if registers.orig_rax == libc::SYS_execve as c_ulonglong
             && registers.rdi > 0
-            && script_pid != *pid
+            && self.parent_pid != pid
         {
-            let path = data_to_string(ptrace_peekdata_iter(*pid, registers.rdi))?;
+            let path = data_to_string(ptrace_peekdata_iter(pid, registers.rdi))?;
             copy("/bin/true", "/tmp/a")?;
-            ptrace_pokedata(*pid, registers.rdi, string_to_data("/tmp/a")?)?;
-            Ok(Some(PathBuf::from(path)))
-        } else {
-            Ok(None)
+            ptrace_pokedata(pid, registers.rdi, string_to_data("/tmp/a")?)?;
+            self.execve_paths.push(PathBuf::from(path.clone()));
         }
-    } else {
-        Ok(None)
+        Ok(())
     }
 }
 
 pub fn execve_paths(executable: &Path) -> R<Vec<PathBuf>> {
-    fork_with_child_errors(
-        || {
-            ptrace::traceme()?;
-            signal::kill(getpid(), Some(Signal::SIGSTOP))?;
-            let path = CString::new(executable.as_os_str().as_bytes())?;
-            execv(&path, &[path.clone()])?;
-            Ok(())
-        },
-        |script_pid: Pid| -> R<Vec<PathBuf>> {
-            let mut result = vec![];
-            waitpid(script_pid, None)?;
-            ptrace::setoptions(
-                script_pid,
-                Options::PTRACE_O_TRACESYSGOOD | Options::PTRACE_O_TRACEFORK,
-            )?;
-            ptrace::syscall(script_pid)?;
-
-            loop {
-                let status = wait()?;
-                if let Some(path) = get_execve_path(&status, script_pid)? {
-                    result.push(path)
-                };
-                match status {
-                    WaitStatus::Exited(pid, ..) => {
-                        if script_pid == pid {
-                            break;
-                        }
-                    }
-                    _ => {
-                        ptrace::syscall(status.pid().unwrap())?;
-                    }
-                }
-            }
-            Ok(result)
-        },
-    )
+    Ok(run_against_mock(executable)?.execve_paths)
 }
 
 #[cfg(test)]
 mod test_execve_paths {
     use super::*;
-    use std::fs::copy;
+    use std::fs;
     use std::process::Command;
+    use tempdir::TempDir;
 
     struct TempFile {
         tempdir: TempDir,
@@ -277,7 +76,7 @@ mod test_execve_paths {
 
     fn write_temp_script(script: &str) -> R<TempFile> {
         let tempfile = TempFile::new()?;
-        write(&tempfile.path(), script.trim_start())?;
+        fs::write(&tempfile.path(), script.trim_start())?;
         run("chmod", vec!["+x", tempfile.path().to_str().unwrap()])?;
         Ok(tempfile)
     }
