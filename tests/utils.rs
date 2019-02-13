9c2a11c06138edf3de33e595789e0733a5b12777
soenkehahn <SoenkeHahn@gmail.com>
2019-02-13 13:38:16 -0500

    output better error messages in high level tests

--- tests/utils.rs
+++ tests/utils.rs
@@ -4,8 +4,14 @@ use tracing_poc::{run, R};
 
 #[allow(dead_code)]
 fn run_high_level_test(directory: &str) -> R<()> {
-    let output = run(&PathBuf::from(directory).join("script"))?;
-    let expected = String::from_utf8(fs::read(PathBuf::from(directory).join("expected"))?)?;
+    let script_file = PathBuf::from(directory).join("script");
+    let output = run(&script_file)
+        .map_err(|error| format!("can't execute {:?}: {}", &script_file, error))?;
+    let expected_file = PathBuf::from(directory).join("expected");
+    let expected = String::from_utf8(
+        fs::read(&expected_file)
+            .map_err(|error| format!("error reading {:?}: {}", &expected_file, error))?,
+    )?;
     assert_eq!(output, expected);
     Ok(())
 }
