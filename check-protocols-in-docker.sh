ea42829df20c38a9f086d43da69ffc2f28ab38ff
soenkehahn <SoenkeHahn@gmail.com>
2019-02-21 18:02:23 -0500

    avoid creating empty directories when running in docker and files are missing

--- check-protocols-in-docker.sh
+++ check-protocols-in-docker.sh
@@ -2,7 +2,10 @@
 
 script=$1
 if [[ "$script" = /* ]]; then
-  docker run --rm -it --cap-add=SYS_PTRACE -v $script:/root/$(basename $script) -v $script.protocols.yaml:/root/$(basename $script).protocols.yaml check-protocols $(basename $script)
+  docker run --rm -it --cap-add=SYS_PTRACE \
+    --mount type=bind,source=$script,target=/root/$(basename $script) \
+    --mount type=bind,source=$script.protocols.yaml,target=/root/$(basename $script).protocols.yaml \
+    check-protocols $(basename $script)
 else
   echo please supply an absolute path
 fi
