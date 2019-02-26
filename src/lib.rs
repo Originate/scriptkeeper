dd3a16b698775c5c14a162710603ffcdeda2aeea
Sönke Hahn <soenkehahn@gmail.com>
2019-02-26 16:22:59 -0500

    make run_main test independent of internal mock executable format

--- src/lib.rs
+++ src/lib.rs
@@ -102,12 +102,15 @@ pub fn run_main(
 #[cfg(test)]
 mod run_main {
     use super::*;
+    use executable_mock::create_mock_executable;
     use std::io::Cursor;
     use test_utils::TempFile;
 
     #[test]
     fn when_passed_executable_mock_flag_behaves_like_executable_mock() -> R<()> {
-        let file = TempFile::write_temp_script(b"first line\nsecond line\n")?;
+        let context = Context::new_test_context();
+        let executable_contents = create_mock_executable(&context, b"foo".to_vec());
+        let file = TempFile::write_temp_script(&executable_contents)?;
         let args = vec![
             "executable-mock".to_string(),
             "--executable-mock".to_string(),
@@ -115,8 +118,8 @@ mod run_main {
         ]
         .into_iter();
         let mut cursor: Cursor<Vec<u8>> = Cursor::new(vec![]);
-        run_main(Context::new_test_context(), args, &mut cursor)?;
-        assert_eq!(String::from_utf8(cursor.into_inner())?, "second line\n");
+        run_main(context, args, &mut cursor)?;
+        assert_eq!(cursor.into_inner(), b"foo");
         Ok(())
     }
 }
