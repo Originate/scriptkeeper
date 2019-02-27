ca7f16fffab87a903df8d7334a7cad981918fadf
Sönke Hahn <soenkehahn@gmail.com>
2019-02-27 15:52:44 -0500

    move code around in syscall_mocking::tracee_memory

--- src/syscall_mocking/tracee_memory.rs
+++ src/syscall_mocking/tracee_memory.rs
@@ -11,6 +11,14 @@ fn cast_to_byte_array(word: c_ulonglong) -> [u8; 8] {
     *ptr
 }
 
+fn cast_to_word(bytes: [u8; 8]) -> c_ulonglong {
+    let void_ptr;
+    unsafe {
+        void_ptr = std::mem::transmute(bytes);
+    }
+    void_ptr
+}
+
 fn peekdata(pid: Pid, address: c_ulonglong) -> R<c_ulonglong> {
     Ok(ptrace::read(pid, address as *mut c_void)? as c_ulonglong)
 }
@@ -60,12 +68,42 @@ pub fn peek_string_array(pid: Pid, address: c_ulonglong) -> R<Vec<String>> {
     Ok(result)
 }
 
-fn cast_to_word(bytes: [u8; 8]) -> c_ulonglong {
-    let void_ptr;
-    unsafe {
-        void_ptr = std::mem::transmute(bytes);
+#[cfg(test)]
+mod peeking {
+    use super::*;
+
+    #[test]
+    fn reads_null_terminated_strings_from_one_word() {
+        let data = vec![[102, 111, 111, 0, 0, 0, 0, 0]]
+            .into_iter()
+            .map(cast_to_word)
+            .map(Ok);
+        assert_eq!(data_to_string(data).unwrap(), "foo");
     }
-    void_ptr
+
+    #[test]
+    fn works_for_multiple_words() {
+        let data = vec![
+            [97, 98, 99, 100, 101, 102, 103, 104],
+            [105, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        .into_iter()
+        .map(cast_to_word)
+        .map(Ok);
+        assert_eq!(data_to_string(data).unwrap(), "abcdefghi");
+    }
+
+    #[test]
+    fn works_when_null_is_on_the_edge() {
+        let data = vec![
+            [97, 98, 99, 100, 101, 102, 103, 104],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ]
+        .into_iter()
+        .map(cast_to_word)
+        .map(Ok);
+        assert_eq!(data_to_string(data).unwrap(), "abcdefgh");
+    }
 }
 
 pub fn pokedata(pid: Pid, address: c_ulonglong, words: c_ulonglong) -> R<()> {
@@ -86,126 +124,84 @@ pub fn string_to_data(string: &str) -> R<c_ulonglong> {
 }
 
 #[cfg(test)]
-mod test {
+mod poking {
     use super::*;
+    use crate::syscall_mocking::fork_with_child_errors;
+    use nix::sys::ptrace::Options;
+    use nix::sys::signal;
+    use nix::sys::signal::Signal;
+    use nix::sys::wait::{waitpid, WaitStatus};
+    use nix::unistd::{execv, getpid};
+    use std::ffi::CString;
+    use test_utils::assert_error;
 
-    mod peeking {
+    mod string_to_data {
         use super::*;
 
         #[test]
-        fn reads_null_terminated_strings_from_one_word() {
-            let data = vec![[102, 111, 111, 0, 0, 0, 0, 0]]
-                .into_iter()
-                .map(cast_to_word)
-                .map(Ok);
-            assert_eq!(data_to_string(data).unwrap(), "foo");
+        fn converts_strings_to_bytes() -> R<()> {
+            assert_eq!(
+                string_to_data("foo")?,
+                cast_to_word([102, 111, 111, 0, 0, 0, 0, 0])
+            );
+            Ok(())
         }
 
         #[test]
-        fn works_for_multiple_words() {
-            let data = vec![
-                [97, 98, 99, 100, 101, 102, 103, 104],
-                [105, 0, 0, 0, 0, 0, 0, 0],
-            ]
-            .into_iter()
-            .map(cast_to_word)
-            .map(Ok);
-            assert_eq!(data_to_string(data).unwrap(), "abcdefghi");
-        }
-
-        #[test]
-        fn works_when_null_is_on_the_edge() {
-            let data = vec![
-                [97, 98, 99, 100, 101, 102, 103, 104],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-            ]
-            .into_iter()
-            .map(cast_to_word)
-            .map(Ok);
-            assert_eq!(data_to_string(data).unwrap(), "abcdefgh");
+        fn errors_on_too_long_strings() -> R<()> {
+            assert_error!(
+                string_to_data("1234567890"),
+                "string_to_data: string too long"
+            );
+            assert_error!(
+                string_to_data("12345678"),
+                "string_to_data: string too long"
+            );
+            assert_eq!(
+                string_to_data("1234567")?,
+                cast_to_word([49, 50, 51, 52, 53, 54, 55, 0])
+            );
+            Ok(())
         }
     }
 
-    mod poking {
-        use super::*;
-        use crate::syscall_mocking::fork_with_child_errors;
-        use nix::sys::ptrace::Options;
-        use nix::sys::signal;
-        use nix::sys::signal::Signal;
-        use nix::sys::wait::{waitpid, WaitStatus};
-        use nix::unistd::{execv, getpid};
-        use std::ffi::CString;
-        use test_utils::assert_error;
-
-        mod string_to_data {
-            use super::*;
-
-            #[test]
-            fn converts_strings_to_bytes() -> R<()> {
-                assert_eq!(
-                    string_to_data("foo")?,
-                    cast_to_word([102, 111, 111, 0, 0, 0, 0, 0])
-                );
+    #[test]
+    fn roundtrip() -> R<()> {
+        let fork_result = fork_with_child_errors(
+            || {
+                ptrace::traceme()?;
+                signal::kill(getpid(), Some(Signal::SIGSTOP))?;
+                let path = CString::new("/bin/true")?;
+                execv(&path, &[path.clone()])?;
                 Ok(())
-            }
+            },
+            |child| -> R<()> {
+                waitpid(child, None)?;
+                ptrace::setoptions(child, Options::PTRACE_O_TRACESYSGOOD)?;
+                ptrace::syscall(child)?;
 
-            #[test]
-            fn errors_on_too_long_strings() -> R<()> {
-                assert_error!(
-                    string_to_data("1234567890"),
-                    "string_to_data: string too long"
-                );
-                assert_error!(
-                    string_to_data("12345678"),
-                    "string_to_data: string too long"
-                );
-                assert_eq!(
-                    string_to_data("1234567")?,
-                    cast_to_word([49, 50, 51, 52, 53, 54, 55, 0])
-                );
-                Ok(())
-            }
-        }
-
-        #[test]
-        fn roundtrip() -> R<()> {
-            let fork_result = fork_with_child_errors(
-                || {
-                    ptrace::traceme()?;
-                    signal::kill(getpid(), Some(Signal::SIGSTOP))?;
-                    let path = CString::new("/bin/true")?;
-                    execv(&path, &[path.clone()])?;
-                    Ok(())
-                },
-                |child| -> R<()> {
-                    waitpid(child, None)?;
-                    ptrace::setoptions(child, Options::PTRACE_O_TRACESYSGOOD)?;
-                    ptrace::syscall(child)?;
-
-                    loop {
-                        let status = waitpid(child, None)?;
-                        match status {
-                            WaitStatus::Exited(..) => {
-                                break;
-                            }
-                            WaitStatus::PtraceSyscall(..) => {
-                                let registers = ptrace::getregs(child)?;
-                                if registers.orig_rax == libc::SYS_execve as c_ulonglong {
-                                    pokedata(child, registers.rdi, string_to_data("/foo")?)?;
-                                    let result =
-                                        data_to_string(peekdata_iter(child, registers.rdi))?;
-                                    assert_eq!(result, "/foo");
-                                }
-                            }
-                            _ => {}
+                loop {
+                    let status = waitpid(child, None)?;
+                    match status {
+                        WaitStatus::Exited(..) => {
+                            break;
                         }
-                        ptrace::syscall(child)?;
+                        WaitStatus::PtraceSyscall(..) => {
+                            let registers = ptrace::getregs(child)?;
+                            if registers.orig_rax == libc::SYS_execve as c_ulonglong {
+                                pokedata(child, registers.rdi, string_to_data("/foo")?)?;
+                                let result = data_to_string(peekdata_iter(child, registers.rdi))?;
+                                assert_eq!(result, "/foo");
+                            }
+                        }
+                        _ => {}
                     }
-                    Ok(())
-                },
-            );
-            assert_error!(fork_result, "ENOENT: No such file or directory");
-            Ok(())
-        }
+                    ptrace::syscall(child)?;
+                }
+                Ok(())
+            },
+        );
+        assert_error!(fork_result, "ENOENT: No such file or directory");
+        Ok(())
     }
 }
