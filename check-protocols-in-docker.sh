e822d81cf9af311a985cc5d4508bab897aaae80a
soenkehahn <SoenkeHahn@gmail.com>
2019-02-28 18:24:38 -0500

    use cwd feature to test check-protocols-in-docker.sh

--- check-protocols-in-docker.sh
+++ check-protocols-in-docker.sh
@@ -2,10 +2,12 @@
 
 script=$1
 if [[ "$script" = /* ]]; then
-  docker run --rm -it --cap-add=SYS_PTRACE \
-    --mount type=bind,source=$script,target=/root/$(basename $script) \
-    --mount type=bind,source=$script.protocols.yaml,target=/root/$(basename $script).protocols.yaml \
-    check-protocols $(basename $script)
+  true
 else
-  echo please supply an absolute path
+  script=$(pwd)/$script
 fi
+
+docker run --rm -it --cap-add=SYS_PTRACE \
+  --mount type=bind,source=$script,target=/root/$(basename $script) \
+  --mount type=bind,source=$script.protocols.yaml,target=/root/$(basename $script).protocols.yaml \
+  check-protocols $(basename $script)
