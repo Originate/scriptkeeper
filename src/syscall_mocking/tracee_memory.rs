bd54c4326024ec6a28b1846dcac2d3afe9f9be19
matt handler <matt.handler@gmail.com>
2019-03-07 14:36:32 -0500

    add ability to poke 4 bytes instead of 8 into child process memory

--- src/syscall_mocking/tracee_memory.rs
+++ src/syscall_mocking/tracee_memory.rs
@@ -3,20 +3,37 @@ use libc::{c_ulonglong, c_void};
 use nix::sys::ptrace;
 use nix::unistd::Pid;
 
-fn cast_to_byte_array(word: c_ulonglong) -> [u8; 8] {
-    let ptr: &[u8; 8];
-    unsafe {
-        ptr = &*(&word as *const u64 as *const [u8; 8]);
-    }
-    *ptr
+fn cast_to_four_byte_array(x: i32) -> [u8; 4] {
+    [
+        (x & 0xff) as u8,
+        ((x >> 8) & 0xff) as u8,
+        ((x >> 16) & 0xff) as u8,
+        ((x >> 24) & 0xff) as u8,
+    ]
 }
 
-fn cast_to_word(bytes: [u8; 8]) -> c_ulonglong {
-    let void_ptr;
-    unsafe {
-        void_ptr = std::mem::transmute(bytes);
-    }
-    void_ptr
+fn cast_to_eight_byte_array(x: c_ulonglong) -> [u8; 8] {
+    [
+        (x & 0xff) as u8,
+        ((x >> 8) & 0xff) as u8,
+        ((x >> 16) & 0xff) as u8,
+        ((x >> 24) & 0xff) as u8,
+        ((x >> 32) & 0xff) as u8,
+        ((x >> 40) & 0xff) as u8,
+        ((x >> 48) & 0xff) as u8,
+        ((x >> 56) & 0xff) as u8,
+    ]
+}
+
+fn cast_to_eight_byte_word(bytes: [u8; 8]) -> c_ulonglong {
+    bytes[0] as c_ulonglong
+        + ((bytes[1] as c_ulonglong) << 8)
+        + ((bytes[2] as c_ulonglong) << 16)
+        + ((bytes[3] as c_ulonglong) << 24)
+        + ((bytes[4] as c_ulonglong) << 32)
+        + ((bytes[5] as c_ulonglong) << 40)
+        + ((bytes[6] as c_ulonglong) << 48)
+        + ((bytes[7] as c_ulonglong) << 56)
 }
 
 fn peekdata(pid: Pid, address: c_ulonglong) -> R<c_ulonglong> {
@@ -45,7 +62,7 @@ fn peekdata_iter(pid: Pid, address: c_ulonglong) -> impl Iterator<Item = R<c_ulo
 fn data_to_string(data: impl Iterator<Item = R<c_ulonglong>>) -> R<Vec<u8>> {
     let mut result = vec![];
     'outer: for word in data {
-        for char in cast_to_byte_array(word?).iter() {
+        for char in cast_to_eight_byte_array(word?).iter() {
             if *char == 0 {
                 break 'outer;
             }
@@ -80,7 +97,7 @@ mod peeking {
     fn reads_null_terminated_strings_from_one_word() {
         let data = vec![[102, 111, 111, 0, 0, 0, 0, 0]]
             .into_iter()
-            .map(cast_to_word)
+            .map(cast_to_eight_byte_word)
             .map(Ok);
         assert_eq!(data_to_string(data).unwrap(), b"foo");
     }
@@ -92,7 +109,7 @@ mod peeking {
             [105, 0, 0, 0, 0, 0, 0, 0],
         ]
         .into_iter()
-        .map(cast_to_word)
+        .map(cast_to_eight_byte_word)
         .map(Ok);
         assert_eq!(data_to_string(data).unwrap(), b"abcdefghi");
     }
@@ -104,13 +121,28 @@ mod peeking {
             [0, 0, 0, 0, 0, 0, 0, 0],
         ]
         .into_iter()
-        .map(cast_to_word)
+        .map(cast_to_eight_byte_word)
         .map(Ok);
         assert_eq!(data_to_string(data).unwrap(), b"abcdefgh");
     }
 }
 
-fn pokedata(pid: Pid, address: c_ulonglong, words: c_ulonglong) -> R<()> {
+pub fn poke_four_bytes(pid: Pid, address: c_ulonglong, small_word: i32) -> R<()> {
+    let existing_word: c_ulonglong = peekdata(pid, address)?;
+    let mut overlapping_data_array: [u8; 8] = cast_to_eight_byte_array(existing_word);
+
+    let first_four_bytes: [u8; 4] = cast_to_four_byte_array(small_word);
+    for i in 0..4 {
+        overlapping_data_array[i] = first_four_bytes[i];
+    }
+
+    let new_word: c_ulonglong = cast_to_eight_byte_word(overlapping_data_array);
+    ptrace::write(pid, address as *mut c_void, new_word as *mut c_void)?;
+
+    Ok(())
+}
+
+pub fn pokedata(pid: Pid, address: c_ulonglong, words: c_ulonglong) -> R<()> {
     ptrace::write(pid, address as *mut c_void, words as *mut c_void)?;
     Ok(())
 }
@@ -128,7 +160,7 @@ fn string_to_data(string: &[u8], max_size: c_ulonglong) -> R<Vec<c_ulonglong>> {
                     word[i] = *char;
                 }
             }
-            result.push(cast_to_word(word));
+            result.push(cast_to_eight_byte_word(word));
         }
         Ok(result)
     }
@@ -171,7 +203,7 @@ mod poking {
         fn converts_strings_to_bytes() -> R<()> {
             assert_eq!(
                 string_to_data(b"foo", 8)?,
-                vec![cast_to_word([102, 111, 111, 0, 0, 0, 0, 0])]
+                vec![cast_to_eight_byte_word([102, 111, 111, 0, 0, 0, 0, 0])]
             );
             Ok(())
         }
@@ -181,8 +213,8 @@ mod poking {
             assert_eq!(
                 string_to_data(b"foo_foo_foo", 16)?,
                 vec![
-                    cast_to_word([102, 111, 111, 95, 102, 111, 111, 95]),
-                    cast_to_word([102, 111, 111, 0, 0, 0, 0, 0]),
+                    cast_to_eight_byte_word([102, 111, 111, 95, 102, 111, 111, 95]),
+                    cast_to_eight_byte_word([102, 111, 111, 0, 0, 0, 0, 0]),
                 ]
             );
             Ok(())
@@ -200,7 +232,7 @@ mod poking {
             );
             assert_eq!(
                 string_to_data(b"1234567", 8)?,
-                vec![cast_to_word([49, 50, 51, 52, 53, 54, 55, 0])]
+                vec![cast_to_eight_byte_word([49, 50, 51, 52, 53, 54, 55, 0])]
             );
             assert_error!(
                 string_to_data(b"123456781234567890", 16),
@@ -213,8 +245,8 @@ mod poking {
             assert_eq!(
                 string_to_data(b"123456781234567", 16)?,
                 vec![
-                    cast_to_word([49, 50, 51, 52, 53, 54, 55, 56]),
-                    cast_to_word([49, 50, 51, 52, 53, 54, 55, 0])
+                    cast_to_eight_byte_word([49, 50, 51, 52, 53, 54, 55, 56]),
+                    cast_to_eight_byte_word([49, 50, 51, 52, 53, 54, 55, 0])
                 ]
             );
             Ok(())
@@ -225,8 +257,8 @@ mod poking {
             assert_eq!(
                 string_to_data(b"12345678", 16)?,
                 vec![
-                    cast_to_word([49, 50, 51, 52, 53, 54, 55, 56]),
-                    cast_to_word([0, 0, 0, 0, 0, 0, 0, 0])
+                    cast_to_eight_byte_word([49, 50, 51, 52, 53, 54, 55, 56]),
+                    cast_to_eight_byte_word([0, 0, 0, 0, 0, 0, 0, 0])
                 ]
             );
             Ok(())
@@ -236,6 +268,13 @@ mod poking {
     mod roundtrip {
         use super::*;
         use libc::user_regs_struct;
+
+        fn cast_to_four_byte_word(bytes: [u8; 4]) -> i32 {
+            bytes[0] as i32
+                + ((bytes[1] as i32) << 8)
+                + ((bytes[2] as i32) << 16)
+                + ((bytes[3] as i32) << 24)
+        }
 
         fn run_roundtrip_test(test: fn(child: Pid, registers: user_regs_struct) -> R<()>) -> R<()> {
             fork_with_child_errors(
@@ -271,6 +310,23 @@ mod poking {
                     Ok(())
                 },
             )
+        }
+
+        #[test]
+        fn roundtrip_for_four_byte_word() -> R<()> {
+            run_roundtrip_test(|child, registers| {
+                let eight_bytes: [u8; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+                let four_bytes: [u8; 4] = [40, 30, 20, 10];
+
+                pokedata(child, registers.rdi, cast_to_eight_byte_word(eight_bytes))?;
+                poke_four_bytes(child, registers.rdi, cast_to_four_byte_word(four_bytes))?;
+
+                assert_eq!(
+                    cast_to_eight_byte_array(peekdata(child, registers.rdi)?),
+                    [40, 30, 20, 10, 5, 6, 7, 8],
+                );
+                Ok(())
+            })
         }
 
         #[test]
