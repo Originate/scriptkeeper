a3b75c70e18e36eebe2324337377a8e252df0e42
soenkehahn <SoenkeHahn@gmail.com>
2019-03-08 17:30:53 -0500

    add tests for cli parsing

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,7 @@
 use crate::R;
 use std::path::PathBuf;
 
+#[derive(Debug, PartialEq)]
 pub enum Args {
     ExecutableMock { executable_mock_path: PathBuf },
     CheckProtocols { script_path: PathBuf },
@@ -19,4 +20,56 @@ pub fn parse_args(mut args: impl Iterator<Item = String>) -> R<Args> {
             script_path: PathBuf::from(argument),
         },
     })
+}
+
+#[cfg(test)]
+mod parse_args {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn returns_the_given_script() -> R<()> {
+        assert_eq!(
+            parse_args(vec!["program", "file"].into_iter().map(String::from))?,
+            Args::CheckProtocols {
+                script_path: PathBuf::from("file")
+            }
+        );
+        Ok(())
+    }
+
+    mod executable_mock {
+        use super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn returns_the_executable_mock_file() -> R<()> {
+            assert_eq!(
+                parse_args(
+                    vec!["program", "--executable-mock", "file"]
+                        .into_iter()
+                        .map(String::from)
+                )?,
+                Args::ExecutableMock {
+                    executable_mock_path: PathBuf::from("file")
+                }
+            );
+            Ok(())
+        }
+
+        #[test]
+        fn allows_arbitrary_arguments() -> R<()> {
+            assert_eq!(
+                parse_args(
+                    vec!["program", "--executable-mock", "file", "foo", "bar"]
+                        .into_iter()
+                        .map(String::from)
+                )?,
+                Args::ExecutableMock {
+                    executable_mock_path: PathBuf::from("file")
+                }
+            );
+            Ok(())
+        }
+    }
 }
